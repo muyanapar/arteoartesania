d0d37addff1ffd33e9344544e09a239ab2f464f7
oms.1001 <muyanaparoficial@gmail.com>
2021-11-04 16:36:27 -0400

    Second test

--- sketch.js
+++ sketch.js
@@ -7,6 +7,8 @@ let artes = 0;
 let artesanias = 0;
 let resultados;
 let json = {}; // new  JSON Object
+var w = window.innerWidth;
+var h = window.innerHeight;  
 
 json.id = 0;
 
@@ -20,16 +22,16 @@ function preload(){
     }
 }
 function setup(){
-    createCanvas(1024, 1024)
+    createCanvas(w, h)
     button = createButton(' ');
-    button.position(canvas.width/2,512);
+    button.position(w/6,512);
     button.style("background","url('imag/Botones-01.png')")
     button.style("padding","14px 65px")
     button.style("display","inline-block")
     button.style("border", "none")
 
     button_2 = createButton(' ');
-    button_2.position(canvas.width/2 + canvas.width/4,512);
+    button_2.position(w/6 + w/2,512);
     button_2.style("background","url('imag/Botones-02.png')")
     button_2.style("padding","14px 65px")
     button_2.style("display","inline-block")
@@ -37,13 +39,13 @@ function setup(){
 }
 function draw(){
     background(bg)
-    image(girafas[ite], canvas.width/3,50,412,412)
+    image(girafas[ite], w/3,50,412,412)
     if (juego_finalizado === 1){
         clear()
         button.style("display","none")
         button_2.style("display","none")
-        image(bg, 0,0,1024,2024)
-        image(gracias, 0,0);
+        image(bg, w/4,0,h,w)
+        image(gracias, w/4,0);
     }
     button.mousePressed(arte);
     button.mouseReleased(arte_sig)
